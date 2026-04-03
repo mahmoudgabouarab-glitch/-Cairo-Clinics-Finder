798878e4c6eef2919fcd8ca845ef93af5a878325
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-04-03 15:09:22 +0200

    add firebase and permission geolocator

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:cairo_clinics_finder/clinics_finder_app.dart';
+import 'package:cairo_clinics_finder/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ClinicsFinderApp());
 }
-
-
-
-
