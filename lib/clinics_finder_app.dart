a6224671dd583c578c0d45657adf8fb0e121bd3a
mahmoudgabouarab-glitch <mahmoud.g.abouarab@icloud.com>
2026-04-03 05:53:05 +0200

    creat splash screen

---
+++ lib/clinics_finder_app.dart
@@ -0,0 +1,22 @@
+import 'package:cairo_clinics_finder/feature/splash/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ClinicsFinderApp extends StatelessWidget {
+  const ClinicsFinderApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 800),
+      child: MaterialApp(
+        theme: ThemeData(
+          scaffoldBackgroundColor: Color(0xFFF0F4F8),
+          appBarTheme: AppBarTheme(backgroundColor: Color(0xFFF0F4F8)),
+        ),
+        debugShowCheckedModeBanner: false,
+        home: const Splash(),
+      ),
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,15 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:cairo_clinics_finder/clinics_finder_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:cairo_clinics_finder/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ClinicsFinderApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
